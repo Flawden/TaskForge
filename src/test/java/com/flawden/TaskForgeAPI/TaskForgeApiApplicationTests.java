5fb8a1bbd9e5bbdadb020e041aad3a2a20952442
Flawden <topchiidv1998@gmail.com>
2025-02-06 19:08:20 +0300

    The basic repository structure has been initialized

---
+++ src/test/java/com/flawden/TaskForgeAPI/TaskForgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flawden.TaskForgeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskForgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
